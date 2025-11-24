95f77051afb5b74c33cc0ffccc05d9e87fe431a6
Vincent-zhangS <vzhang2022@gmail.com>
2025-11-24 10:33:14 -0500

    everything from class

--- laughingman.js
+++ laughingman.js
@@ -11,20 +11,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     let box = document.querySelector("#box");
 
-    for(let i = 0; i<SQUARE_COUNT; i++){
-        //create the square
-        let square = document.createElement('img');
-        square.src = 'laughing_man.jpg';
-        square.alt = 'Catch the Laughing Man!';
-        square.className = 'square';
-        box.appendChild(square);
-        //add it to the screen
-    }
-    let allSquares = box.children;
-    let squareArray = Array.from(children);
-    squareArray.forEach(function(element, index){
-        var dx = SPEED * (Math.random()*2 -1);
-        var dy = SPEED * (Math.random()*2 -1);
-    });
+for(let i = 0; i<SQUARE_COUNT;i++){
+    //create the square
+    let square = document.createElement('img');
+    square.src = 'laughing_man.jpg';
+    square.alt = 'Catch the Laughing Man!';
+    square.className = 'square';
+    box.appendChild(square);
+    //add it to the screen
+}
 
+let allSquares = box.children;
+let squareArray = Array.from(allSquares);
+squareArray.forEach(function(element, index) {
+    var dx = SPEED * Math.random() * 2 - 1;
+    var dy = SPEED * Math.random() * 2 - 1;
+    setInterval(() => {
+        let currentLeft = parseInt(element.style.left) || 225;
+        let currentTop = parseInt(element.style.top) || 175;
+        if (currentLeft >= 450 || currentLeft <= 0) {
+            dx *= -1;
+            element.style.borderColor = getRandomColor();
+        }
+        if (currentTop >= 350 || currentTop <= 0) {
+            dy *= -1;
+            element.style.borderColor = getRandomColor();
+        }
+        element.style.left = (currentLeft + dx) + "px";
+        element.style.top = (currentTop + dy) + "px";
+    }, TIMER_SPEED);
+
+    });
 });
+function getRandomColor() {
+    let r = parseInt(Math.random() * 256);
+    let g = parseInt(Math.random() * 256);
+    let b = parseInt(Math.random() * 256);
+    let color = `rgb(${r},${g},${b})`;
+    return color;
+}
